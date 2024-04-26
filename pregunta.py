c11d4d88442e714d6cec404c696e9247506d0ffc
jromog11 <jromog@unal.edu.co>
2024-04-25 19:57:49 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -6,7 +6,15 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+
 import pandas as pd
+
+
+def convertir_a_formato_fecha(fecha):
+    try:
+        return pd.to_datetime(fecha, format="%d/%m/%Y")
+    except:
+        return pd.to_datetime(fecha, format="%Y/%m/%d")
 
 
 def clean_data():
@@ -16,5 +24,28 @@ def clean_data():
     #
     # Inserte su código aquí
     #
+
+    df = df.copy()
+    df = df.dropna()
+    df = df.drop(columns=["Unnamed: 0"])
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+    df["idea_negocio"] = df["idea_negocio"].str.lower()
+    df["barrio"] = df["barrio"].str.lower()
+    df["línea_credito"] = df["línea_credito"].str.lower()
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    df["estrato"] = df["estrato"].astype(int)
+    df["fecha_de_beneficio"] = df["fecha_de_beneficio"].apply(convertir_a_formato_fecha)
+    df["monto_del_credito"] = df["monto_del_credito"].str.replace(
+        r"[^\d.]", "", regex=True
+    )
+    df["monto_del_credito"] = pd.to_numeric(df["monto_del_credito"], errors="coerce")
+    df["idea_negocio"] = df["idea_negocio"].str.replace("_", " ")
+    df["barrio"] = df["barrio"].str.replace("_", " ")
+    df["idea_negocio"] = df["idea_negocio"].str.replace("-", " ")
+    df["barrio"] = df["barrio"].str.replace("-", " ")
+    df["línea_credito"] = df["línea_credito"].str.replace("_", " ")
+    df["línea_credito"] = df["línea_credito"].str.replace("-", " ")
+    df = df.drop_duplicates()
 
     return df
